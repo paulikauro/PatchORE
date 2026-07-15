8775870f7a96beb68b2a07f88dcc34b801c1186c
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2026-07-15 17:53:51 +0300

    Use sendInfo in packet listeners too

--- src/main/kotlin/org/openredstone/patchore/patch/PlayerInventoryPatch.kt
+++ src/main/kotlin/org/openredstone/patchore/patch/PlayerInventoryPatch.kt
@@ -4,12 +4,10 @@ import com.comphenix.protocol.PacketType
 import com.comphenix.protocol.events.ListenerPriority
 import com.comphenix.protocol.events.PacketAdapter
 import com.comphenix.protocol.events.PacketEvent
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.NamedTextColor
-import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 import org.openredstone.patchore.PatchORE
+import org.openredstone.patchore.sendInfo
 
 class PlayerInventoryPatch(plugin: JavaPlugin) :
     PacketAdapter(plugin, ListenerPriority.HIGHEST, PacketType.Play.Client.ENTITY_NBT_QUERY) {
@@ -32,18 +30,6 @@ class PlayerInventoryPatch(plugin: JavaPlugin) :
 
     private fun clearNBT(player: Player) {
         player.inventory.clear()
-
-        player.sendMessage(
-            Component.text("[", NamedTextColor.DARK_GRAY)
-                .append(Component.text(plugin.name, NamedTextColor.GRAY))
-                .append(Component.text("]", NamedTextColor.DARK_GRAY))
-                .append(
-                    Component.text(
-                        "Your inventory was cleared due to an NBT overload.",
-                        NamedTextColor.GOLD,
-                        TextDecoration.BOLD
-                    )
-                )
-        )
+        player.sendInfo("Your inventory was cleared due to an NBT overload.")
     }
 }
--- src/main/kotlin/org/openredstone/patchore/patch/PlayerPositionPatch.kt
+++ src/main/kotlin/org/openredstone/patchore/patch/PlayerPositionPatch.kt
@@ -4,12 +4,9 @@ import com.comphenix.protocol.PacketType
 import com.comphenix.protocol.events.ListenerPriority
 import com.comphenix.protocol.events.PacketAdapter
 import com.comphenix.protocol.events.PacketEvent
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.NamedTextColor
-import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
-import org.openredstone.patchore.PatchORE
+import org.openredstone.patchore.sendInfo
 import kotlin.math.abs
 
 class PlayerPositionPatch(plugin: JavaPlugin) :
@@ -41,19 +38,7 @@ class PlayerPositionPatch(plugin: JavaPlugin) :
 
     private fun fixPlayer(player: Player) {
         player.teleport(player.world.spawnLocation)
-
-        player.sendMessage(
-            Component.text("[", NamedTextColor.DARK_GRAY)
-                .append(Component.text(plugin.name, NamedTextColor.GRAY))
-                .append(Component.text("] ", NamedTextColor.DARK_GRAY))
-                .append(
-                    Component.text(
-                        "You were sent back to spawn due to an invalid location.",
-                        NamedTextColor.GOLD,
-                        TextDecoration.BOLD
-                    )
-                )
-        )
+        player.sendInfo("You were sent back to spawn due to an invalid location.")
     }
 
     private companion object {
